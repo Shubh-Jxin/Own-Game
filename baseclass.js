72097c617c159a12797c3ca7bea2b43234260394
Shubh Jain <65031443+Shubh-Jxin@users.noreply.github.com>
2020-08-02 13:59:23 +0530

    Add files via upload

--- baseclass.js
+++ baseclass.js
@@ -1,21 +1,18 @@
 class Baseclass{
-    constructor(x,y,r,angle){
+    constructor(x,y,w,h,image){
         var options={
             isStatic:true
         }
-        this.body= Bodies.circle(x,y,r,options);
-        this.r=r;
-        Matter.Body.setAngle(this.body,angle)
-        World.add(world, this.body);
-        
+        this.body= Bodies.rectangle(x,y,w,h,options);
+        this.w=w;
+        this.h=h;
+        this.image= loadImage(image);
+        World.add(world,this.body);
     }
 
     display(){
         var pos= this.body.position;
-        var angle= this.body.angle;
-        ellipseMode(RADIUS);
-        ellipse(pos.x,pos.y,this.r);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.w,this.h);
     }
-
 }
-
